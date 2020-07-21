bdab3921686562a23e3f3cd95e8beafe8faf1d49
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2020-07-21 17:47:49 -0300

    Add converter in java

---
+++ converter.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class converter {
+    public static void main(String[] args) {
+        
+        String numero_binario = "";
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.print("Digite um número binario: ");
+        numero_binario = teclado.nextLine();
+
+        if (numero_binario.equals(""))
+            System.out.println("Digite algum valor");
+        else if (validate_input(numero_binario))
+            System.out.println("Digite somente 0's e 1's");
+        else
+            System.out.println("Decimal: "+bin2dec(numero_binario));
+        
+        teclado.close();
+
+    }
+
+    // Returns false if the binary String not contains anything other than 0 or 1
+    private static boolean validate_input(String bin) {
+        
+        boolean flag = false;
+
+        for (int aux = 0; aux < bin.length(); aux++) {
+            if (bin.split("")[aux].compareTo("1") > 0)
+                flag = true;
+        }
+        return flag;
+    }
+
+    private static int bin2dec(String bin)
+    {
+        int decimal = 0;
+        for (int aux = bin.length() - 1; aux >= 0; aux--)
+        {
+            decimal += Integer.parseInt(bin.split("")[aux]) 
+                * Math.pow(2, bin.length() - 1 - aux);
+        }
+
+        return decimal;
+    }
+
+}
